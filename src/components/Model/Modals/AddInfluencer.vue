2dff1c9e6e9e8ec1a7d955a676a31c668aecbdcc
Ted Hwang <tedchwang@gmail.com>
2019-09-28 15:28:29 +0800

    Sync selected target node in ModalNodeSelection.vue to parent component AddInfluencer.vue.

---
+++ src/components/Model/Modals/AddInfluencer.vue
@@ -0,0 +1,62 @@
+<template>
+  <q-card>
+    <modal-header>
+      <template v-slot:header>Add Influencer</template>
+    </modal-header>
+    <q-form @submit.prevent="submitForm">
+      <q-card-section>
+        <modal-node-selection
+          :targetNodeId.sync="linkToSubmit.targetNodeId"
+          ref="modalNodeSelection"
+        />
+      </q-card-section>
+      <modal-buttons />
+    </q-form>
+  </q-card>
+</template>
+
+<script>
+import { mapState, mapActions } from "vuex";
+
+export default {
+  components: {
+    "modal-header": require("components/Shared/ModalComponents/ModalHeader.vue")
+      .default,
+    "modal-buttons": require("components/Shared/ModalComponents/ModalButtons.vue")
+      .default,
+    "modal-node-selection": require("components/Model/Modals/Shared/ModalNodeSelection.vue")
+      .default
+  },
+  data() {
+    return {
+      linkToSubmit: {
+        sourceNodeId: this.selectedNodeId,
+        targetNodeId: "",
+        targetType: "influencer"
+      }
+    };
+  },
+  computed: {
+    ...mapState("model", ["selectedNodeId"])
+  },
+  methods: {
+    ...mapActions("model", ["addLink"]),
+
+    submitForm() {
+      this.$refs.modalNodeSelection.$refs.targetNode.validate();
+      if (!this.$refs.modalNodeSelection.$refs.targetNode.hasError) {
+        this.submitLink();
+      }
+    },
+
+    submitLink() {
+      console.log(this.linkToSubmit);
+      this.addLink({
+        link: this.linkToSubmit,
+        teamId: this.$route.params.teamId
+      });
+      this.$emit("close");
+    }
+  }
+};
+</script>
---
+++ src/components/Model/Modals/Shared/ModalNodeSelection.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="row q-mb-sm">
+    <q-select
+      filled
+      label="Influencer"
+      style="width: 250px"
+      autofocus
+      :value="targetNodeId"
+      use-input
+      input-debounce="0"
+      :options="options"
+      option-value="id"
+      option-label="name"
+      @filter="filterFn"
+      :rules="[val => !!val || 'Field is required']"
+      emit-value
+      map-options
+      @input="$emit('update:targetNodeId', $event)"
+      ref="targetNode"
+    >
+      <template v-slot:no-option>
+        <q-item>
+          <q-item-section class="text-grey">No results</q-item-section>
+        </q-item>
+      </template>
+    </q-select>
+  </div>
+</template>
+
+<script>
+import { mapState, mapGetters } from "vuex";
+
+export default {
+  props: ["targetNodeId"],
+  data() {
+    return {
+      options: this.nodeOptions,
+      selected: this.targetNodeId
+    };
+  },
+  computed: {
+    ...mapState("model", ["selectedNodeId"]),
+    ...mapGetters("model", ["nodes"]),
+    nodeOptions() {
+      return this.nodes.filter(node => node.id != this.selectedNodeId);
+    }
+  },
+  methods: {
+    filterFn(val, update) {
+      if (val === "") {
+        update(() => {
+          this.options = this.nodeOptions;
+        });
+        return;
+      }
+
+      update(() => {
+        const needle = val.toLowerCase();
+        this.options = this.nodeOptions.filter(
+          node => node.name.toLowerCase().indexOf(needle) > -1
+        );
+      });
+    }
+  }
+};
+</script>
