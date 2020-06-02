91a5fae0f492c149ee99095800ac91038c0ebaf2
Ted Hwang <tedchwang@gmail.com>
2020-06-02 16:32:32 +0800

    Remove dead code.

--- src/mixins/mixin-add-edit-impact.js
+++
@@ -1,9 +0,0 @@
-import { mapState } from "vuex";
-import { createHelpers } from "vuex-map-fields";
-
-const { mapFields } = createHelpers({
-  getterType: "uiAction/getField",
-  mutationType: "uiAction/updateUiActionField"
-});
-
-export default {};
