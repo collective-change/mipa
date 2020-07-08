8f83c4757470788b589b9a083405e5d268641e3f
Ted Hwang <tedchwang@gmail.com>
2020-07-08 17:50:39 +0800

    Remove dead code.

--- src/components/Model/DependencyGraph.vue
+++ src/components/Model/DependencyGraph.vue
@@ -543,25 +543,6 @@ export default {
         .on("mouseover", this.nodeMouseOver)
         .on("mouseout", this.nodeMouseOut)
         .on("contextmenu", function (d) {
-          Function.prototype.clone = function () {
-            var cloneObj = this;
-            if (this.__isClone) {
-              cloneObj = this.__clonedFrom;
-            }
-
-            var temp = function () {
-              return cloneObj.apply(this, arguments);
-            };
-            for (var key in this) {
-              temp[key] = this[key];
-            }
-
-            temp.__isClone = true;
-            temp.__clonedFrom = cloneObj;
-
-            return temp;
-          };
-
           d3.event.preventDefault();
           that.nodeClick(d, null, "contextMenuClick");
 
