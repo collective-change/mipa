415ec35f0eb1fd4a4684842bb27a9d082e394c17
Ted Hwang <tedchwang@gmail.com>
2020-10-07 16:49:17 +0800

    translation for Total ROI

--- src/components/Actions/ActionsList.vue
+++ src/components/Actions/ActionsList.vue
@@ -151,7 +151,7 @@ export default {
           name: "totalRoi",
           required: true,
           align: "right",
-          label: "Total ROI",
+          label: "總 ROI",
           field: "totalRoi",
           format: val => `${formatNumber(val, 2)}`,
           sortable: true,
