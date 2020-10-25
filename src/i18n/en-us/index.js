6d6ca03311a041e0c53621c7c3ebf4ed88f91661
Ted Hwang <tedchwang@gmail.com>
2020-10-25 16:53:22 +0800

    Add translations

--- src/i18n/en-us/index.js
+++ src/i18n/en-us/index.js
@@ -23,7 +23,7 @@ export default {
   Privacy: "Privacy",
   "Policy & Safety": "Policy & Safety",
 
-  //for linkGroups
+  // for linkGroups
   "My dashboard": "My dashboard",
   "To do": "To do",
   Organization: "Organization",
@@ -55,5 +55,31 @@ export default {
   "My current focus": "My current focus",
   "To do": "To do",
   "Time log": "Time log",
-  "My team's work": "My team's work"
+  "My team's work": "My team's work",
+
+  // for ActionsList.vue
+  Add: "Add",
+  Recalculate: "Recalculate",
+  Leverage: "Leverage",
+  Status: "Status",
+  Title: "Title",
+  isProject: "Is project",
+  marginalTotalBenefitNpv: "Marginal total benefit NPV",
+  marginalTotalCostNpv: "Marginal total cost NPV",
+  marginalNetTotalBenefitNpv: "Marginal net total benefit NPV",
+  outstandingDirectCost: "Outstanding direct cost",
+  totalRoi: "Total ROI",
+  effortCompletionPercentage: "Effort completion",
+  relationships: "Relationships",
+  dueDate: "Due date",
+
+  // for ActionSummary.vue
+  Notes: "Notes",
+  effortCostPerHour: "Effort cost per hour",
+  customEffortCostPerHr: "Custom effort cost per hour",
+  estEffortHrs: "Estimated hours",
+  percentDone: "% done",
+  estSpending: "Estimated spending",
+  spentAmount: "Spent amount"
+
 };
