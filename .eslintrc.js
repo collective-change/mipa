1b5a98a2bf4722c08fec630720874d82ad0a33d1
Ted Hwang <tedchwang@gmail.com>
2020-11-01 09:07:02 +0800

    add eslint-plugin-quasar and .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+{
+  "plugins": [
+    "quasar"
+  ],
+    "extends": [
+    "plugin:quasar/standard"
+  ],
+    "rules": [
+    'quasar/no-invalid-props': "error",
+    'quasar/no-invalid-qfield-usage': "error"
+  ]
+}
