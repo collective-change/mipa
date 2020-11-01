dea3a15768679075375d69a6d66302c992301a43
Ted Hwang <tedchwang@gmail.com>
2020-11-01 09:40:14 +0800

    Fix syntax

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,8 @@
-{
-  "plugins": [
-    "quasar"
-  ],
-    "extends": [
-    "plugin:quasar/standard"
-  ],
-    "rules": [
-    'quasar/no-invalid-props': "error",
-    'quasar/no-invalid-qfield-usage': "error"
-  ]
-}
+module.exports = {
+  plugins: ["quasar"],
+  extends: ["plugin:quasar/standard"],
+  rules: {
+    "quasar/no-invalid-props": "error",
+    "quasar/no-invalid-qfield-usage": "error"
+  }
+};
