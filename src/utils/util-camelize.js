0e5644f7e0d60429141b2d401905d12c1b7f9833
Ted Hwang <tedchwang@gmail.com>
2020-12-10 09:59:30 +0800

    fix camelize function (#56)

--- src/utils/util-camelize.js
+++ src/utils/util-camelize.js
@@ -1,7 +1,8 @@
 export function camelize(str) {
   return str
+    .toLowerCase()
     .replace(/(?:^\w|[A-Z]|\b\w)/g, function(word, index) {
       return index === 0 ? word.toLowerCase() : word.toUpperCase();
     })
-    .replace(/\s|\(|\)|\[|\]+/g, "");
+    .replace(/\s|-|\(|\)|\[|\]+/g, "");
 }
